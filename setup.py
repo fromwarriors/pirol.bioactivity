36109af0c38c5ff12e0f88fae47b7ba09cd9dca2
Michael Oselskyi <oselskyi@gmail.com>
2013-08-10 15:07:27 -0700

    Update setup.py
    -Small formatting changes

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # (c) 2013 Maciej Szymkiewicz
 # Licensed under the MIT license: http://opensource.org/licenses/MIT
+
 from setuptools import setup, find_packages
 
 version = None
